44dda9d78edc8acce7abd7a75d5145fa929746b1
diosadandi <diosp2000@gmail.com>
2024-05-03 19:35:36 +0700

    push semua code perubahan

---
+++ src/pages/ItemPage.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar"
+
+const ItemPage = () =>{
+
+    return(
+        <section className="bg-colorPicker-krem dark:bg-white-900 items-center">
+            <Navbar/>
+            <div className="items-center py-54">
+                <h1>Create Item</h1>
+            </div>
+            
+        </section>
+    )
+}
+
+export default ItemPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      colorPicker : {
+        biruBG : '#008DDA',
+        krem : '#F7EEDD',
+        submit: '#41C9E2',
+        cancel : '#ACE2E1',
+        logout : '#ACE2E1',
+      }
+
+    },
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require('flowbite/plugin')],
 }
