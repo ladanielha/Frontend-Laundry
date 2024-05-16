db551b83ffad9fc2c0a5a1464c2d2be5c84defdf
randytjioe <randytjioe95@gmail.com>
2024-05-16 21:33:57 +0700

    fix error

--- src/components/TransactionDetail.jsx
+++ src/components/TransactionDetail.jsx
@@ -145,8 +145,8 @@ export default function TransactionDetail() {
                       </td>
                       <td className="px-2 py-4 text-center ">
                         <button
-                          data-modal-target="popup-modal-status-loundry"
-                          data-modal-toggle="popup-modal-status-loundry"
+                          data-modal-target="popupmodal-status-loundry"
+                          data-modal-toggle="popupmodal-status-loundry"
                           type="button"
                         >
                           {transaction.statusLaundry === "Process" ? (
@@ -252,7 +252,7 @@ export default function TransactionDetail() {
           </div>
 
           <div
-            id="popup-modal-status-loundry"
+            id="popupmodal-status-loundry"
             tabIndex={-1}
             className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
           >
@@ -261,7 +261,7 @@ export default function TransactionDetail() {
                 <button
                   type="button"
                   className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                  data-modal-hide="popup-modal"
+                  data-modal-hide="popupmodal-status-loundry"
                 >
                   <svg
                     className="w-3 h-3"
@@ -300,14 +300,14 @@ export default function TransactionDetail() {
                     Are you sure you want to delete this product?
                   </h3>
                   <button
-                    data-modal-hide="popup-modal"
+                    data-modal-hide="popupmodal-status-loundry"
                     type="button"
                     className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                   >
                     Yes, I'm sure
                   </button>
                   <button
-                    data-modal-hide="popup-modal"
+                    data-modal-hide="popupmodal-status-loundry"
                     type="button"
                     className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                   >
